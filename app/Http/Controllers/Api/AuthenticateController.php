fcabf672ac652c7530f18c6058622cbcd35584da
andrei.k <andrei.k.ysbm@gmail.com>
2017-05-03 11:58:14 +0300

    Add cross domain request

--- app/Http/Controllers/Api/AuthenticateController.php
+++ app/Http/Controllers/Api/AuthenticateController.php
@@ -22,7 +22,7 @@ class AuthenticateController extends Controller
     {
         // grab credentials from the request
         $credentials = $request->all('email', 'password');
-
+        print_r($credentials);
         try {
             // attempt to verify the credentials and create a token for the user
             if (! $token = JWTAuth::attempt($credentials)) {
