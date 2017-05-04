380ef0cd543ef5dd57a2f5e7e2f0639a7d4d5a23
andrei.k <andrei.k.ysbm@gmail.com>
2017-05-04 15:43:03 +0300

    Retreiving the Authenticated user from a token

--- app/Http/Controllers/Api/AuthenticateController.php
+++ app/Http/Controllers/Api/AuthenticateController.php
@@ -10,7 +10,6 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use JWTAuth;
-use Tymon\JWTAuth\Exceptions\JWTException;
 use App\User;
 use Illuminate\Http\Request;
 
@@ -39,6 +38,32 @@ class AuthenticateController extends Controller
 
         // all good so return the token
         return response()->json(compact(['token', 'user'] ));
+    }
+
+    public function getAuthenticatedUser()
+    {
+        try {
+
+            if (! $user = JWTAuth::parseToken()->authenticate()) {
+                return response()->json(['user_not_found'], 404);
+            }
+
+        } catch (TokenExpiredException $e) {
+
+            return response()->json(['token_expired'], $e->getStatusCode());
+
+        } catch (TokenInvalidException $e) {
+
+            return response()->json(['token_invalid'], $e->getStatusCode());
+
+        } catch (JWTException $e) {
+
+            return response()->json(['token_absent'], $e->getStatusCode());
+
+        }
+
+        // the token is valid and we have found the user via the sub claim
+        return response()->json(compact('user'));
     }
 
 }
