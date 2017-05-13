e4d4a9de3e570efbf9e7c65a8169ed087986799f
andrei.k <andrei.k.ysbm@gmail.com>
2017-05-13 14:31:59 +0300

    Cheked CORS and Autintificate v1

--- app/Api/Controllers/AuthenticateController.php
+++ app/Api/Controllers/AuthenticateController.php
@@ -29,9 +29,9 @@ class AuthenticateController extends BaseController
     public function authenticate(Request $request)
     {
         // grab credentials from the request
-        $credentials = $request->only('email', 'password');
-
-        //$credentials = json_decode($request->getContent(), true);
+        //$credentials = $request->only('email', 'password');
+        // this work only for cors for angular
+        $credentials = json_decode($request->getContent(), true);
         try {
             // attempt to verify the credentials and create a token for the user
             if (! $token = JWTAuth::attempt($credentials)) {
