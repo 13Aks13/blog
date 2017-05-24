a91ad79839c792eb691ee9ede618623466028bbb
andrei.k <andrei.k.ysbm@gmail.com>
2017-05-24 18:55:52 +0300

    User update

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,15 +15,24 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name')->collate('utf8_bin');;
-            $table->string('email')->unique()->collate('utf8_bin');;
-            $table->string('avatar')->nullable()->collate('utf8_bin');;
-            $table->string('password')->collate('utf8_bin');;
+            $table->string('first name')->collate('utf8_bin');
+            $table->string('flastst name')->collate('utf8_bin');
+            $table->string('email')->unique()->collate('utf8_bin');
+            $table->string('avatar')->nullable()->collate('utf8_bin');
+            $table->string('password')->collate('utf8_bin');
+
+            $table->string('birthday')->nullable()->collate('utf8_bin');
+            $table->string('skype')->nullable()->collate('utf8_bin');
+            $table->string('phone')->nullable()->collate('utf8_bin');
+            $table->string('title')->nullable()->collate('utf8_bin');
+
             $table->integer('role_id')->unsigned()->default(3);
-//            $table->integer('status_id')->unsigned()->default(1);
+            $table->integer('location_id')->unsigned()->default(0);
+            $table->integer('workstation')->nullable();
+
             $table->boolean('isBlocked')->default(false);
             $table->integer('goal')->default(0);
-            $table->string('remember_token')->default(0)->collate('utf8_bin');;
+            $table->string('remember_token')->default(0)->collate('utf8_bin');
             $table->timestamps();
         });
     }
